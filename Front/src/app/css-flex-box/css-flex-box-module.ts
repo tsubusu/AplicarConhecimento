bd2905c843378d9eb641dac756882f8e7d6e064c
jorge.pitang <jorge.ori@pitang.com>
2022-06-29 21:38:38 -0300

    criação da pagina de picos

--- Front/src/app/css-flex-box/css-flex-box-module.ts
+++ Front/src/app/css-flex-box/css-flex-box-module.ts
@@ -4,14 +4,17 @@ import { CommonModule } from '@angular/common';
 import { CssFlexBoxRoutingModule } from './css-flex-box-routing.module';
 import { RouterModule, Routes } from '@angular/router';
 import { Aula1Component } from './aula1/aula1.component';
+import { InicioComponent } from './aula1/inicio/inicio.component';
+import { PicosComponent } from './aula1/picos/picos.component';
  
 const routes: Routes = [
-  { path: '', component: CssFlexBoxComponent,
-    // children:[
-    //   { path : 'aula1', component: Aula1Component }
-    // ],
-  },
-  {path: 'aula1', component: Aula1Component,}
+  { path: '', component: CssFlexBoxComponent },
+  {path: 'aula1', component: Aula1Component,
+      children: [
+      { path : 'inicio', component: InicioComponent },
+      { path : 'picos', component: PicosComponent }
+    ],
+  }
 ];
 
 
