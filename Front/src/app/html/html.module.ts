5e93a090ade45ee36b371601cddb9ebcf1362be6
jorge.pitang <jorge.ori@pitang.com>
2022-07-05 22:23:05 -0300

    acessibilidade

--- Front/src/app/html/html.module.ts
+++ Front/src/app/html/html.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HtmlComponent } from './html.component';
+import { Aula1Component } from './aula1/aula1.component';
 
 const routes: Routes = [
-  { path: '', component: HtmlComponent }
+  { path: '', component: HtmlComponent },
+  { path: 'aula1', component: Aula1Component }
 ];
 
 @NgModule({
